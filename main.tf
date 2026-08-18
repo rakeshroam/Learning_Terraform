c0af3f9d009b0b5180c5ef6b0d051e2dbf32a0c4
RAKESH KUMAR <58475973+rakeshroam@users.noreply.github.com>
2026-08-18 11:46:50 +0530

    Modify AMI filters and add architecture/root device filters
    Updated AMI filter values and added new filters for architecture and root device type.

--- main.tf
+++ main.tf
@@ -3,12 +3,22 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = ["bitnami-tomcat-*-debian-12-*"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
   }
 
   owners = ["612051870749"] # Bitnami
@@ -18,7 +28,13 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
 
+  # Add these for production use:
+  # subnet_id              = aws_subnet.main.id
+  # vpc_security_group_ids = [aws_security_group.web.id]
+  # key_name               = "your-key-pair"
+
   tags = {
     Name = "HelloWorld"
   }
 }
+
